1b1aa78d55442c6009847325a7371d2541d43397
University Account <greekharvarddev@gmail.com>
2023-09-14 03:48:48 +0300

    car images complete

--- app/page.tsx
+++ app/page.tsx
@@ -14,24 +14,16 @@ export default async function Home() {
 
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
-          <h1 className="text-4xl font-extrabold">
-            Car Catalogue
-          </h1>
-          <p>
-            Explore the cars you might like
-          </p>
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
         </div>
 
         <div className="home__filters">
           <SearchBar />
 
           <div className="home__filter-container">
-            <CustomFilter 
-              title="fuel"
-            />
-            <CustomFilter 
-              title="year"
-            />
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
           </div>
         </div>
 
@@ -39,22 +31,17 @@ export default async function Home() {
           <section>
             <div className="home__cars-wrapper">
               {allCars?.map((car) => (
-                <CarCard 
-                  car={car}
-                />
+                <CarCard car={car} />
               ))}
             </div>
           </section>
-        ): (
+        ) : (
           <div className="home__error-container">
-            <h2 className="text-black text-xl font-bold">
-              Oops, no results
-            </h2>
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
         )}
-
       </div>
     </main>
-  )
+  );
 }
